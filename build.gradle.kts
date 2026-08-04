c32c38fd6e6975076e013acdd5faa8f9f68f5b9b
Jinzo <91829829+pernio@users.noreply.github.com>
2026-08-04 23:22:04 +0200

    Fix versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ tasks {
         // Configure the Minecraft version for our task.
         // This is the only required configuration besides applying the plugin.
         // Your plugin's jar (or shadowJar if present) will be used automatically.
-        minecraftVersion("1.21.11")
+        minecraftVersion("1.21")
         jvmArgs("-Xms2G", "-Xmx2G")
     }
 
